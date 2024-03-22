898868114daa212305c4e2497632881897ae1bdc
Deobaba <adexsquare4192@gmail.com>
2024-03-22 11:24:19 +0100

    balance and incomeExpense

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,38 @@
 
 <template>
   <Header />
+  <div class="container">
+    <Balance :total = "total" />
+    <IncomeExpenses  :income="income"  :expense="expense"/>
+    <TransactionList :transactions ="transactions" />
+    <AddTransaction />
+  </div>
 </template>
 
 <script setup>
 import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpenses from './components/IncomeExpenses.vue'
+import TransactionList from './components/TransactionList.vue'
+import AddTransaction from './components/AddTransaction.vue'
+import { ref, computed } from 'vue'
+
+const transactions =ref( [
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }])
+
+const total = computed(()=>{
+  return transactions.value.reduce((acc, transaction) => (acc += transaction.amount), 0).toFixed(2)
+})
+
+const income = computed(()=>{
+  return transactions.value.filter(transaction => transaction.amount > 0).reduce((acc, transaction) => (acc += transaction.amount), 0).toFixed(2)
+})
+
+const expense = computed(()=>{
+  return transactions.value.filter(transaction => transaction.amount < 0).reduce((acc, transaction) => (acc += transaction.amount), 0).toFixed(2)
+})
+
 </script>
